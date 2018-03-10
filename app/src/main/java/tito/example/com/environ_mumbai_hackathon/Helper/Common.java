9d308c60153c3313a55a1994c215863c379feefa
Tito <titonadar06@gmail.com>
2018-03-10 11:09:28 +0530

    dependencies of networking done

--- app/src/main/java/tito/example/com/environ_mumbai_hackathon/Helper/Common.java
+++ app/src/main/java/tito/example/com/environ_mumbai_hackathon/Helper/Common.java
@@ -5,5 +5,5 @@ package tito.example.com.environ_mumbai_hackathon.Helper;
  */
 
 public class Common {
-
+    private static String Base_Url="https://api.data.gov.in/resource/4d4250cb-e7dd-4f50-a3f7-c564a0533844?format=json&api-key=579b464db66ec23bdd000001a7acbad520184374518be6f67ffe8515";
 }
---
+++ app/src/main/java/tito/example/com/environ_mumbai_hackathon/Interface/WaterService.java
@@ -0,0 +1,10 @@
+package tito.example.com.environ_mumbai_hackathon.Interface;
+
+/**
+ * Created by tito on 10/3/18.
+ */
+
+public interface WaterService {
+
+
+}
